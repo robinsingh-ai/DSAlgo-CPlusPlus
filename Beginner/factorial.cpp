3802d039fb9c918d02922a5889264268bacf2672
robin025 <robin25tech@gmail.com>
2020-11-28 23:23:38 +0530

    added factorial

---
+++ Beginner/factorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void fact(int n)
+{
+
+    int factorial = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        factorial *= i;
+    }
+
+    cout << "Factorial of " << n << "! : " << factorial;
+}
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    fact(n);
+
+    return 0;
+}
