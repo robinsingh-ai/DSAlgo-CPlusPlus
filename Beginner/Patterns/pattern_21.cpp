8f0b0de3ba7f4fad280fcfe9c965fc8d3bd9d35c
robin025 <robin25tech@gmail.com>
2020-12-25 22:59:31 +0530

    zig zag pattern

---
+++ Beginner/Patterns/pattern_21.cpp
@@ -0,0 +1,38 @@
+/*Pattern 21 : Number Pattern with full pyramid
+
+Example:
+
+    *       *       *       *       *       *       *
+  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *
+*       *       *       *       *       *       *       *
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of stars";
+    cin >> n;
+
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (((i + j) % 4 == 0) || (i == 2 && j % 4 == 0))
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
